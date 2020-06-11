dbfd33d2c1deba8490cf66fc49d40fa5d20524b5
Alba Gustems <albagustems@gmail.com>
2020-06-11 19:33:30 +0200

    ready to use

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express        = require("express");
-const session        = require("express-session");
-const MongoStore     = require("connect-mongo")(session);
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const expressLayouts = require("express-ejs-layouts");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
 
 // Controllers
-const siteController     = require("./routes/siteController");
+const siteController = require("./routes/siteController");
 const locationController = require("./routes/locationController");
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/deploy-exercise");
+mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Middlewares configuration
 app.use(logger("dev"));
@@ -28,15 +33,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Authentication
 app.use(session({
   secret: "deploy-exercise",
-  cookie: { maxAge: 60000 },
+  cookie: {
+    maxAge: 60000
+  },
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60   // 1 day
+    ttl: 24 * 60 * 60 // 1 day
   })
 }));
 app.use(cookieParser());
